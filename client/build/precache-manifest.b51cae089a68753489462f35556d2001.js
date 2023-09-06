a0885fb2f71fe461355e983994a6eda60adb7355
tomasgsantos <tomasgsantos@gmail.com>
2023-09-06 12:37:06 +0100

    frontend

---
+++ client/build/precache-manifest.b51cae089a68753489462f35556d2001.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e50459bece08b455724",
+    "url": "/static/css/main.161579d3.chunk.css"
+  },
+  {
+    "revision": "7e50459bece08b455724",
+    "url": "/static/js/main.85a89a4e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "050a4ba7c945b076e892",
+    "url": "/static/css/2.f51ae970.chunk.css"
+  },
+  {
+    "revision": "050a4ba7c945b076e892",
+    "url": "/static/js/2.a264443b.chunk.js"
+  },
+  {
+    "revision": "ab39a3747eceeb52295837d8f117494a",
+    "url": "/static/media/copdeck_logo.ab39a374.png"
+  },
+  {
+    "revision": "6151a4ebc63f73fd8210290153f5ca3c",
+    "url": "/static/media/doctor_dashboard.6151a4eb.png"
+  },
+  {
+    "revision": "2bb0e3cb4bfd8e3ca51058c643b7e0b5",
+    "url": "/index.html"
+  }
+];
